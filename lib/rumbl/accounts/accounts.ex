28504afc59dee5037bed1c6a2d62197178e692d2
David Obonyo <olengedavid@gmail.com>
2021-06-25 17:28:29 +0300

    Creating users and hashing passwords

---
+++ lib/rumbl/accounts/accounts.ex
@@ -0,0 +1,41 @@
+defmodule Rumbl.Accounts do
+    @moduledoc  """
+    The Accounts context
+    """
+    alias Rumbl.Accounts.User
+    alias Rumbl.Repo
+
+
+
+    def get_user(id) do
+        Repo.get(User, id)
+    end
+
+    def get_user!(id) do
+        Repo.get(User, id)
+    end
+
+    def get_user_by(params) do
+        Repo.get_by(User, params)
+    end
+
+    def list_users() do
+        Repo.all(User)
+    end
+
+    def create_user(attrs \\ %{}) do
+       %User{}
+       |>User.changeset(attrs)
+       |>Repo.insert()
+    end
+
+    def change_user(%User{} = user) do
+        User.changeset(user, %{})
+    end
+
+    def register_user(attrs \\ %{}) do
+        %User{}
+        |> User.registration_changeset(attrs)
+        |> Repo.insert()
+     end
+end
