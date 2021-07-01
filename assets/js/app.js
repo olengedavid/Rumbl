3af1087058d1e603422ad5e2505c00e94b7057a3
David Obonyo <olengedavid@gmail.com>
2021-07-01 10:26:00 +0300

    edited the player.js file and the regular expression to enable video playing

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,4 @@
-import Player from "./player"
-let video = document.getElementById("video")
 
-if(video) {
-    Player.init(video.id, video.getAttribute("data-player-id"), () =>{
-        console.log("player read!")
-    })
-}
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
@@ -22,5 +15,12 @@ import "../css/app.scss"
 //
 import "phoenix_html"
 
+import Player from "./player"
+let video = document.getElementById("video")
 
+if(video) {
+    Player.init(video.id, video.getAttribute("data-player-id"), () =>{
+        console.log("player read!")
+    })
+}
 
